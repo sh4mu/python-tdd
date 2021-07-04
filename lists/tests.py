56fa2c3288bd3c7ec30ad451e3aabf14ca45757c
João Coucelo <nb20651@novabase.pt>
2021-07-04 19:00:18 +0100

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,10 @@
 from lists.models import Item
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
+from lists.views import home_page
+from lists.models import Item
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -19,3 +23,49 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+    
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_only_save_a_POST_not_empty(self):
+        response = self.client.post('/', data={'item_text': ''})
+        self.assertEqual(Item.objects.count(), 0)    
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        #self.assertContains(response, 'itemey 1')
+        #self.assertContains(response, 'itemey 2')
+        
--- lists/views.py
+++ lists/views.py
@@ -8,11 +8,17 @@ def home_page(request):
         new_item_text = request.POST['item_text']
         if new_item_text:
             Item.objects.create(text=new_item_text)
-            return redirect('/')
-    else:
-        new_item_text=''
+            return redirect('/lists/the-only-list-in-the-world/')
     
     items = Item.objects.all()
-    return render(request, 'home.html', {
-        'items': items
-    })
+    return render(request, 'home.html')
+
+def view_list(request):
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        if new_item_text:
+            Item.objects.create(text=new_item_text)
+            return redirect('/lists/the-only-list-in-the-world/')
+            
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
