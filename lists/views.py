05c0ae62e9e96980801ab6e8d75815d052a77417
João Coucelo <nb20651@novabase.pt>
2021-07-04 19:11:17 +0100

    Add a URL for creating a new list via POST

--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,6 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        if new_item_text:
-            Item.objects.create(text=new_item_text)
-            return redirect('/lists/the-only-list-in-the-world/')
-    
-    items = Item.objects.all()
     return render(request, 'home.html')
 
 def view_list(request):
@@ -22,3 +15,7 @@ def view_list(request):
             
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
